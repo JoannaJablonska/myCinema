ae72ed831f8c0b06c492516c6608138a131c1c1f
Łukasz <lukasz.slazyk@lhsystems.com>
2023-07-26 14:36:30 +0200

    Initial

---
+++ mycinema-infrastructure/src/main/java/pl/training/mycinema/infrastructure/MyCinemaInfrastructureApplication.java
@@ -0,0 +1,13 @@
+package pl.training.mycinema.infrastructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCinemaInfrastructureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCinemaInfrastructureApplication.class, args);
+	}
+
+}
---
+++ mycinema-infrastructure/src/test/java/pl/training/mycinema/infrastructure/MyCinemaInfrastructureApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.training.mycinema.infrastructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCinemaInfrastructureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
