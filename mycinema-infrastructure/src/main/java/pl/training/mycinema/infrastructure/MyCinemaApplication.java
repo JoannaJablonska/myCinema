e7ebb17254faf7575d0f952c0958ad5cf4aacaa4
Łukasz <lukasz.slazyk@lhsystems.com>
2023-07-27 16:56:41 +0200

    Added todos

--- mycinema-infrastructure/src/main/java/pl/training/mycinema/infrastructure/MyCinemaApplication.java
+++ mycinema-infrastructure/src/main/java/pl/training/mycinema/infrastructure/MyCinemaApplication.java
@@ -10,4 +10,7 @@ public class MyCinemaApplication {
 		SpringApplication.run(MyCinemaApplication.class, args);
 	}
 
+	//TODO GetScreenings
+	//TODO GetSeats (for the selected screening) e.g. byId
+	//TODO Business Rules
 }
